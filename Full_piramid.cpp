981ebf4cba6562112a3bac7071849dd47ed38749
SANJEEV <159331593+SANJEEVQWERTY@users.noreply.github.com>
2024-04-25 23:06:13 +0530

    Add files via upload

---
+++ Full_piramid.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of n is : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n*2+1;j++){
+            if(j>n-i && j<n+i){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
